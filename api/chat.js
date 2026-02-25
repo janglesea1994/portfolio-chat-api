b2bd890bbe87a9a4836b35c7e9db988676019b35
janglesea1994 <jack.anglesea@dootrix.com>
2026-02-25 21:25:55 +0000

    Change export syntax from default to module.exports

--- api/chat.js
+++ api/chat.js
@@ -1,7 +1,7 @@
 // api/chat.js — Deploy this to Vercel
 // Set ANTHROPIC_API_KEY in your Vercel project environment variables
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
